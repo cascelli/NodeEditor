1b50ed12f680b16bbdce4f823ab103758ece1cbe
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2024-02-14 15:23:38 -0300

    16: Introduction to Serialization

--- node_socket.py
+++ node_socket.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+from node_serializable import Serializable
 from node_graphics_socket import QDMGraphicsSocket
 
 
@@ -9,8 +11,9 @@ RIGHT_BOTTOM = 4
 DEBUG = False
 
 
-class Socket():
+class Socket(Serializable):
     def __init__(self, node, index=0, position=LEFT_TOP, socket_type=1):
+        super().__init__()
 
         self.node = node
         self.index = index
@@ -40,3 +43,14 @@ class Socket():
     def hasEdge(self):
         return self.edge is not None
 
+    def serialize(self):
+        return OrderedDict ([
+            ('id', self.id),
+            ('index', self.index),
+            ('position', self.position),
+            ('socket_type', self.socket_type)
+        ])
+
+    def deserialize(self, data, hashmap={}):
+        print("deserializating data", data)
+        return False
